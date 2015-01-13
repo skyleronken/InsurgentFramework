16f5a7757fc30cf67b58f7dfdeafd6bdda085c62
skyleronken <skyler.onken@gmail.com>
2015-01-13 13:03:00 +0000

    Added the beginning of controllers

--- base.py
+++ base.py
@@ -1,17 +1,47 @@
 #!/usr/bin/env python
 
+from controller import Controller
+import time
+from random import randint
+
 # TODO:
 # - Prevent replay
-#
+# - Add active day and active hour calculation to calculate_sleep()
 #
 
-from controller import Controller
+MIN_SLEEP_INT = 60
+MAX_SLEEP_INT = 600
+ACTIVE_DAYS = ('M','T','W','Th','F','Sa','Su')
+ACTIVE_HOURS = ('0001','2359')
+
+# global flag
+continue_beacon = True
+
+def calculate_sleep():
+
+    #cur_time = time.ctime()
+    sleep_time = 0
+
+    sleep_time += randint(MIN_SLEEP_INT,MAX_SLEEP_INT)
+    
+    return sleep_time
+    
+
+def start_beacon_loop(controller):
+    
+    while continue_beacon:
+        
+        controller.beacon()
+        time.sleep(calculate_sleep())
+        
+    return True
+                
 
 def main():
     
     controller = Controller()
     
+    start_beacon_loop(controller)
 
 if __name__ == "__main__":
     main()
-    
--- controller.py
+++ controller.py
@@ -4,17 +4,58 @@
 # This is the application controller that manages the calls through all the different types of modules
 #
 
+# TODO:
+#
+# - Parse XML to build mappings
+#
+
 class Controller:
     
+    beacon_map = {}
+    decoder_map = {}
+    command_map = {}
+    response_map = {}
+    
     def __init__(self):
-        pass
-    
-    
-    def handle_communication(self):
-        # this function will receive information from the socket and call the handlers
-        pass
+        self.build_handlers()
+        
     
     def build_handlers(self):
         # this function is used by the constructor to setup the dictionaries with the command to command object mapping.
+        
+        # this is temporary. In the long run should read in XML to parse all mappings.
+        # Pass in strings? possible to avoid object types to save the headache of dynamic imports? Makes hard part just retrieving class by reflection, rather than reference.
         pass
+    
+    def handle_beacon(self):
+        pass
+    
+    def handle_decode(self, encoded_data):
+        pass
+    
+    def handle_command(self, command, params):
+        pass
+    
+    def handle_response(self, results):
+        pass
+    
+    def handle(self):
+        
+        # Send command to beacon handler
+        success, encoded_data = self.handle_beacon()
+        
+        # Send response to decoders
+        success, decoded_data = self.handle_decode(encoded_data)
+        
+        # Process command
+        command, params = decoded_data
+        success, results = self.handle_command(command, params)
+        
+        # Send response
+        success = self.handle_response(results)
+    
+    def beacon(self):
+        # facade
+        self.handle()
+    
     
